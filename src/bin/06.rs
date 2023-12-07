13f9d7e9d438a50aabc4e33488471fbea4899ef0
Kleidi Bujari <mail@kleidi.ca>
2023-12-06 22:12:57 -0500

    complete: day 6

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,6 +1,5 @@
 fn main() {
-    let input = "Time:      7  15   30
-Distance:  9  40  200" /* include_str!("../../input/06.txt") */
+    let input = include_str!("../../input/06.txt")
         .lines()
         .map(|line| line.split_whitespace().skip(1))
         .map(|line| line.map(|item| item.parse::<usize>().unwrap()));
@@ -13,8 +12,27 @@ Distance:  9  40  200" /* include_str!("../../input/06.txt") */
 
     let p1 = input
         .into_iter()
-        .map(|(time, distance)| time)
+        .map(|(time, distance)| {
+            (1..time)
+                .map(|windup| windup * (time - windup))
+                .filter(|dist| *dist > distance)
+                .count()
+        })
         .product::<usize>();
 
-    println!("{p1:?}");
+    let input2: [usize; 2] = include_str!("../../input/06.txt")
+        .lines()
+        .map(|line| line.split_once(':').unwrap().1)
+        .map(|line| line.split_whitespace().collect::<String>())
+        .map(|line| line.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>()
+        .try_into()
+        .unwrap();
+
+    let p2 = (1..input2[0])
+        .map(|windup| windup * (input2[0] - windup))
+        .filter(|dist| *dist > input2[1])
+        .count();
+
+    println!("{p1} {p2}");
 }
